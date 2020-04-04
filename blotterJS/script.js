4e4e7782f584829a69a7149ba1bb7e92d8e052f5
Martin Diatta <58808297+Diatemba99@users.noreply.github.com>
2020-04-04 03:42:46 +0000

    Add files via upload

---
+++ blotterJS/script.js
@@ -0,0 +1,27 @@
+
+
+var container = document.getElementById("main");
+
+var text = new Blotter.Text("CODEGRID",{
+
+   family: "serif",
+   size: 150,
+   fill: "#fff",
+   paddingLeft:80,
+   paddingRight:80,
+   paddingTop:80,
+   paddingBottom:80
+});
+
+var material = new Blotter.FliesMaterial();
+material.uniforms.uPointCellWidth.value = 0.01;
+material.uniforms.uPointRadius.value = 0.6;
+material.uniforms.uSpeed.value = 3;
+
+
+var blotter = new Blotter(material,{
+	texts: text
+});
+
+var scope = blotter.forText(text);
+scope.appendTo(container);
